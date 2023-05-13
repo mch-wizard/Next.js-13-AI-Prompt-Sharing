4ed6aafc3ae0277af2800692481c42f1dd260471
Marcin <mch.wizard@gmail.com>
2023-05-14 00:11:49 +0200

    user.js file done, next.config.js file fixed, sign in with google done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
-import { signIn } from "next-auth/react";
 
+import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
@@ -12,15 +12,32 @@ const handler = NextAuth({
         })
     ],
     async session({ session }) {
+        // store the user id from MongoDB to session
+        const sessionUser = await User.findOne({
+            email: session.user.email
+        });
+
+        session.user.id = sessionUser._id.toString();
         
+        return session;
     },
     async signIn({ profile }) {
         try {
             await connectToDB();
 
             // check if a user already exists
+            const userExists = await User.findOne({
+                email: profile.email
+            });
 
             // if not, create a new user
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture,
+                })
+            }
 
             return true;
         } catch (error) {
